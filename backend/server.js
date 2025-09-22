404cbf66a868ab6d4c2218a4ddfcba280c33100e
techteam-web <techteam@brainwing.in>
2025-09-22 13:40:09 +0530

    adding prod url

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,8 @@ server.timeout = 120000;          // 2 minutes request timeout
 
 const allowedOrigins = process.env.ALLOWED_ORIGINS
   ? process.env.ALLOWED_ORIGINS.split(',')
-  : ['http://localhost:3000', 'http://localhost:5173'];
+  : ['http://localhost:3000', 'http://localhost:5173', 'https://runwal-webpage.vercel.app', // Add your production frontend URL
+      'https://inventory-page-ashen.vercel.app' ];
 
 app.use(helmet());
 
